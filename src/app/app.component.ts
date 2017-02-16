ceb3d0302bba34c465168503b22c5ff94735f3e7
sbesh91 <s.beshensky@gmail.com>
2017-02-16 00:10:09 -0600

    using angular's animation suite instead of transitions now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,15 +21,18 @@ export class AppComponent {
       I'm speaking with myself, number one, because I have a very good brain and I've said a lot of things. I think the only card she has is the Lorem card. The best taco bowls are made in Trump Tower Grill. I love Hispanics!
       I think the only card she has is the Lorem card. Some people have an ability to write placeholder text... It's an art you're basically born with. You either have it or you don't.
       I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. All of the words in Lorem Ipsum have flirted with me - consciously or unconsciously. That's to be expected. The concept of Lorem Ipsum was created by and for the Chinese in order to make U.S. design jobs non-competitive.`,
-      active:false
+      active:false,
+      state:"inactive"
     },
     {
       text:"",
-      active:false
+      active:false,
+      state:"inactive"
     },
     {
       text:"",
-      active:false
+      active:false,
+      state:"inactive"
     }
   ];
 }
--- src/app/card.component.ts
+++ src/app/card.component.ts
@@ -5,15 +5,58 @@ import {
   state,
   style,
   transition,
-  animate 
+  animate,
+  keyframes
 } from '@angular/core';
 
 @Component({
   selector: 'card',
   templateUrl: './card.component.html',  
-  styleUrls: ['./card.component.css'],
+  styleUrls: ['./card.component.css'],  
+  animations: [   
+    trigger('dataState', [
+      state('inactive', style({
+        backgroundColor: '#eee',
+        transform: 'scale(.8)'
+      })),
+      state('active',   style({
+        backgroundColor: '#cfd8dc',
+        transform: 'scale(1.1)'
+      })),
+      transition('inactive => active', [
+        animate('300ms cubic-bezier(0.4, 0.0, 0.2, 1)', keyframes([
+          style({transform: 'scale(.8)', offset: 0}),
+          style({transform: 'scale(1)', offset: 0.3}),
+          style({transform: 'scale(1.1)', offset: 1.0})
+        ]))
+      ]),      
+      transition('active => inactive', [
+        animate('300ms cubic-bezier(0.4, 0.0, 0.2, 1)', keyframes([
+          style({transform: 'scale(1.1)', offset: 0}),
+          style({transform: 'scale(1)', offset: 0.3}),  
+          style({transform: 'scale(.8)', offset: 1.0}),                  
+        ]))
+      ]),      
+      transition('active => inactive', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class Card { 
     @Input()
-    data: Object = {};
+    data:{
+      text: string,
+      active: boolean,
+      state: string
+    };
+    toggleState() {
+      this.data.state = (this.data.state === 'active' ? 'inactive' : 'active');
+      this.data.active = this.data.state === 'active';
+    };
+    animationStarted(e){
+      console.log(e);
+    };
+    animationDone(e){
+      console.log(e);
+    };
+
 }
