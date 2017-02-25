b745642d9ad1161d9a5f261dad507483d3a1f7cc
sbesh91 <s.beshensky@gmail.com>
2017-02-24 23:05:36 -0600

    hover zoom finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,17 +22,36 @@ export class AppComponent {
       I think the only card she has is the Lorem card. Some people have an ability to write placeholder text... It's an art you're basically born with. You either have it or you don't.
       I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. All of the words in Lorem Ipsum have flirted with me - consciously or unconsciously. That's to be expected. The concept of Lorem Ipsum was created by and for the Chinese in order to make U.S. design jobs non-competitive.`,
       active:false,
-      state:"inactive"
+      state:"void"
     },
     {
-      text:"",
+      text:`
+      I'm speaking with myself, number one, because I have a very good brain and I've said a lot of things. I think the only card she has is the Lorem card. The best taco bowls are made in Trump Tower Grill. I love Hispanics!
+      I think the only card she has is the Lorem card. Some people have an ability to write placeholder text... It's an art you're basically born with. You either have it or you don't.
+      I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. All of the words in Lorem Ipsum have flirted with me - consciously or unconsciously. That's to be expected. The concept of Lorem Ipsum was created by and for the Chinese in order to make U.S. design jobs non-competitive.`,
       active:false,
-      state:"inactive"
+      state:"void"
     },
     {
-      text:"",
+      text:`
+      I'm speaking with myself, number one, because I have a very good brain and I've said a lot of things. I think the only card she has is the Lorem card. The best taco bowls are made in Trump Tower Grill. I love Hispanics!
+      I think the only card she has is the Lorem card. Some people have an ability to write placeholder text... It's an art you're basically born with. You either have it or you don't.
+      I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. All of the words in Lorem Ipsum have flirted with me - consciously or unconsciously. That's to be expected. The concept of Lorem Ipsum was created by and for the Chinese in order to make U.S. design jobs non-competitive.`,
       active:false,
-      state:"inactive"
+      state:"void"
+    },
+    {
+      text:`
+      I'm speaking with myself, number one, because I have a very good brain and I've said a lot of things. I think the only card she has is the Lorem card. The best taco bowls are made in Trump Tower Grill. I love Hispanics!
+      I think the only card she has is the Lorem card. Some people have an ability to write placeholder text... It's an art you're basically born with. You either have it or you don't.
+      I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. All of the words in Lorem Ipsum have flirted with me - consciously or unconsciously. That's to be expected. The concept of Lorem Ipsum was created by and for the Chinese in order to make U.S. design jobs non-competitive.`,
+      active:false,
+      state:"void"
     }
   ];
+  get any(){
+    return this.cards.some((i)=>{
+      return i.active;
+    });
+  }
 }
--- src/app/card.component.ts
+++ src/app/card.component.ts
@@ -6,7 +6,9 @@ import {
   style,
   transition,
   animate,
-  keyframes
+  keyframes,
+  OnChanges,
+  SimpleChanges
 } from '@angular/core';
 
 @Component({
@@ -15,6 +17,10 @@ import {
   styleUrls: ['./card.component.css'],  
   animations: [   
     trigger('dataState', [
+      state('void', style({
+        backgroundColor: '#eee',
+        transform: 'scale(1)'
+      })),
       state('inactive', style({
         backgroundColor: '#eee',
         transform: 'scale(.8)'
@@ -26,18 +32,40 @@ import {
       transition('inactive => active', [
         animate('300ms cubic-bezier(0.4, 0.0, 0.2, 1)', keyframes([
           style({transform: 'scale(.8)', offset: 0}),
-          style({transform: 'scale(1)', offset: 0.3}),
+          
           style({transform: 'scale(1.1)', offset: 1.0})
         ]))
       ]),      
-      transition('active => inactive', [
+      transition('active => void', [
         animate('300ms cubic-bezier(0.4, 0.0, 0.2, 1)', keyframes([
           style({transform: 'scale(1.1)', offset: 0}),
-          style({transform: 'scale(1)', offset: 0.3}),  
+          style({transform: 'scale(1)', offset: 1.0})      
+        ]))
+      ]),
+      transition('inactive => void', [
+        animate('300ms cubic-bezier(0.4, 0.0, 0.2, 1)', keyframes([
+          style({transform: 'scale(.8)', offset: 0}),
+          style({transform: 'scale(1)', offset: 1.0})      
+        ]))
+      ]),
+      transition('void => active', [
+        animate('300ms cubic-bezier(0.4, 0.0, 0.2, 1)', keyframes([
+          style({transform: 'scale(1)', offset: 0}),
+          style({transform: 'scale(1.1)', offset: 1.0})
+        ]))
+      ]),
+      transition('void => inactive', [
+        animate('300ms cubic-bezier(0.4, 0.0, 0.2, 1)', keyframes([
+          style({transform: 'scale(1)', offset: 0}),
+          style({transform: 'scale(.8)', offset: 1.0})
+        ]))
+      ]), 
+      transition('active => inactive', [
+        animate('300ms cubic-bezier(0.4, 0.0, 0.2, 1)', keyframes([
+          style({transform: 'scale(1.1)', offset: 0}),          
           style({transform: 'scale(.8)', offset: 1.0}),                  
         ]))
       ]),      
-      transition('active => inactive', animate('100ms ease-out'))
     ])
   ]
 })
@@ -48,15 +76,24 @@ export class Card {
       active: boolean,
       state: string
     };
-    toggleState() {
-      this.data.state = (this.data.state === 'active' ? 'inactive' : 'active');
+    @Input() 
+    any: boolean;
+    ngOnChanges(changes: SimpleChanges) {            
+      if(this.data.state !== "active" && this.any)
+      {
+        this.data.state = "inactive";
+      } else if(!this.any && !this.data.active) {
+        this.data.state = "void";
+      }
+    }
+    toggleState(e) {
+      this.data.state = (this.data.state === 'active' ? 'void' : 'active');      
       this.data.active = this.data.state === 'active';
     };
     animationStarted(e){
-      console.log(e);
+      
     };
     animationDone(e){
-      console.log(e);
-    };
-
+      
+    };    
 }
